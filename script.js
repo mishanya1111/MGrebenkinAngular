7aa7c5a0e38671a63a6ba15433ce9ee094f23a77
Michael <mihail0grebenkin@gmail.com>
2024-06-14 18:23:39 +0300

    добавлена функция range и sum. Можно уменшить код в функции range.

--- script.js
+++ script.js
@@ -2,33 +2,35 @@ start();
 
 function start() {
     while (true) {
-        var selectItem = prompt('Введите номер задания которое хотите выполнить:\n' +
+        let selectItem = prompt('Введите номер задания которое хотите выполнить:\n' +
             '1.Треугольник\n' +
             '2.Шахматы\n' +
             '3.Минимальный элемент\n' +
             '4.Чётность\n' +
             '5.Количество B\n' +
             '6.Количество определённой буквы в заданной строке\n' +
+            '7.Сумму элементов массива(2)\n' +
+            '8.Сумму элементов массива(3)\n' +
             '0.Выход');
         switch (selectItem) {
             case '1': {
-                var height = prompt('Введите высоту треугольника');
+                let height = prompt('Введите высоту треугольника');
                 alert(forLoop(height));
                 break;
             }
             case '2': {
-                var width = prompt('Введите ширину доски');
+                let width = prompt('Введите ширину доски');
                 alert(chess(width));
                 break;
             }
             case '3': {
-                var num1 = prompt('Введите первый элемент');
-                var num2 = prompt('Введите второй элемент');
+                let num1 = prompt('Введите первый элемент');
+                let num2 = prompt('Введите второй элемент');
                 alert("минимальное между  " + num1 + " и " + num2 + " это:" + min(num1, num2));
                 break;
             }
             case '4': {
-                var num = prompt('Введите число');
+                let num = prompt('Введите число');
                 if (isEven(num)) {
                     alert("Число" + num + " чётное");
                 } else alert("Число " + num + " нечётное");
@@ -42,12 +44,33 @@ function start() {
             case '6': {
                 let line = prompt('Введите строку');
                 let char = prompt('Введите символ, кторый будет подсчитан');
-                alert("Количество букв " + char  + " в заданной строке: " + countChar(line, char));
+                alert("Количество букв " + char + " в заданной строке: " + countChar(line, char));
                 break;
             }
+            case '7': {
+                let num1 = Number(prompt('Введите первый элемент'));
+                let num2 = Number(prompt('Введите второй элемент'));
+                let arr = range(num1, num2);
+                alert("массив " + arr);
+                alert("сумма:" + sum(arr))
+                break;
+            }
+            case '8': {
+                let num1 = Number(prompt('Введите первый элемент'));
+                let num2 = Number(prompt('Введите второй элемент'));
+                let num3 = Number(prompt('Введите шаг'));
+                let arr = range(num1, num2, num3);
+                alert("массив " + arr);
+                alert("сумма:" + sum(arr))
+                break;
+            }
+
             case '0': {
                 return 0;
             }
+            default:
+                return 0;
+
 
         }
     }
@@ -55,8 +78,8 @@ function start() {
 
 
 function forLoop(n) {
-    var tempHashtag = "#", result = "";
-    for (var i = 0; i < n; i++) {
+    let tempHashtag = "#", result = "";
+    for (let i = 0; i < n; i++) {
         result += tempHashtag.repeat(i + 1) + "\n";
     }
     return result;
@@ -64,9 +87,9 @@ function forLoop(n) {
 
 
 function chess(n) {
-    var result = "";
-    for (var i = 0; i < n; i++) {
-        for (var j = 0; j < n; j++) {
+    let result = "";
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
             if ((i + j) % 2 === 0) {
                 result += '#';
             } else result += ' ';
@@ -86,13 +109,57 @@ function isEven(n) {
 }
 
 function countBs(line) {
-    return countChar(line,"B");
+    return countChar(line, "B");
 }
-function countChar(line,char) {
-    var count = 0;
+
+function countChar(line, char) {
+    let count = 0;
     for (let i = 0; i < line.length; i++) {
         if (line[i] === char) count++;
     }
     return count;
 }
 
+/*
+Write a range() function that takes two arguments, start and end,
+and returns an array containing all the numbers from start up to (and including) end.
+Next, write a sum() function that takes an array of numbers and returns the sum
+ of these numbers. Run the previous program and see whether it does indeed return 55.
+Also, modify your range() function to take an optional third argument
+that indicates the “step” value used to build up the array. If no step is given,
+the array elements go up by increments of one, corresponding to the old behavior.
+ The function call range(1, 10, 2) should return [1, 3, 5, 7, 9].
+  Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+ */
+function range(num1, num2, step = 1) {
+    let arr = [], i = 0;
+    if (step === 0) return arr;
+    if (step > 0) {
+        while (true) {
+            if (num2 < num1) {
+                break;
+            }
+            arr[i] = num1;
+            num1 += step;
+            i++;
+        }
+    } else {
+        while (true) {
+            if (num2 > num1) {
+                break;
+            }
+            arr[i] = num1;
+            num1 += step;
+            i++;
+        }
+    }
+    return arr;
+}
+
+function sum(arr) {
+    let sumNum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sumNum += arr[i];
+    }
+    return sumNum;
+}
