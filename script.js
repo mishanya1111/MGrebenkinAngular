63878c34d448d97185fed33a536186bc015188d6
Michael <mihail0grebenkin@gmail.com>
2024-06-14 15:43:08 +0300

    Переделано всё управлние. Теперь оно использует prompt/alert для выюоа желаемого задания. Также измененеы функции forLoop и chess, и добавлены функции min и isEven(в прошлом комите первое появление).

--- script.js
+++ script.js
@@ -1,59 +1,75 @@
+start();
+
+function start() {
+    while (true) {
+        var selectItem = prompt('Введите номер задания которое хотите выполнить:\n' +
+            '1.Треугольник\n' +
+            '2.Шахматы\n' +
+            '3.Минимальный элемент\n' +
+            '4.Чётность\n' +
+            '5.Выход');
+        switch (selectItem) {
+            case '1': {
+                var height = prompt('Введите высоту треугольника');
+                alert(forLoop(height));
+                break;
+            }
+            case '2': {
+                var width = prompt('Введите ширину доски');
+                alert(chess(width));
+                break;
+            }
+            case '3': {
+                var num1 = prompt('Введите первый элемент');
+                var num2 = prompt('Введите второй элемент');
+                alert("min between " + num1 + " and " + num2 + " is:" + min(num1, num2));
+                break;
+            }
+            case '4': {
+                var num = prompt('Введите число');
+                if (isEven(num)) {
+                    alert("Число" + num + " чётное");
+                } else alert("Число " + num + " нечётное");
+                break;
+            }
+            case '5': {
+                return 0;
+            }
+
+        }
+    }
+}
+
+
 function forLoop(n) {
     var tempHashtag = "#", result = "";
     for (var i = 0; i < n; i++) {
-        result += tempHashtag.repeat(i+1) + "\n";
+        result += tempHashtag.repeat(i + 1) + "\n";
     }
     return result;
 }
 
-console.log(forLoop(4));
 
 function chess(n) {
     var result = "";
-
     for (var i = 0; i < n; i++) {
-        var j = 0, check = true;
-        if (i % 2 === 1) {
-            result += " ";
-            j++;
+        for (var j = 0; j < n; j++) {
+            if ((i + j) % 2 === 0) {
+                result += '#';
+            } else result += ' ';
         }
-        while (j < n) {
-            if (check) {
-                result += "#";
-                check = false;
-            } else {
-                result += " ";
-                check = true;
-            }
-            j++
-        }
-        result += "\n";
-
-
+        result += '\n';
     }
     return result;
 }
 
-console.log("chess:\n" + chess(4));
 
 
-function min(a , b) {
-    if( a < b) return a;
+function min(a, b) {
+    if (a < b) return a;
     return b;
 }
-
-var a= 3, b = 4;
-console.log("min between " + a + " and " + b + " is:" + min(a,b));
-
-a= 5.1;
-b = -1.2;
-console.log("min between " + a + " and " + b + " is:" + min(a,b));
 
 function isEven(n) {
     return n % 2 === 0;
 }
-var num = 1;
-console.log("The number " + num + " is even? Answer: " + isEven(num) );
-
-num = 2;
-console.log("The number " + num + " is even? Answer: " + isEven(num) );
