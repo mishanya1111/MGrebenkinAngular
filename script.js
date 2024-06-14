10a6903b9e888d62e31d5cc31682485115c6d01b
Michael <mihail0grebenkin@gmail.com>
2024-06-14 15:04:15 +0300

    Исправлен forLoop, добавлены функции min и isEven

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function chess(n) {
 
     for (var i = 0; i < n; i++) {
         var j = 0, check = true;
-        if (i % 2 == 1) {
+        if (i % 2 === 1) {
             result += " ";
             j++;
         }
@@ -34,4 +34,26 @@ function chess(n) {
     return result;
 }
 
-console.log("chess:\n" + chess(15));
+console.log("chess:\n" + chess(4));
+
+
+function min(a , b) {
+    if( a < b) return a;
+    return b;
+}
+
+var a= 3, b = 4;
+console.log("min between " + a + " and " + b + " is:" + min(a,b));
+
+a= 5.1;
+b = -1.2;
+console.log("min between " + a + " and " + b + " is:" + min(a,b));
+
+function isEven(n) {
+    return n % 2 === 0;
+}
+var num = 1;
+console.log("The number " + num + " is even? Answer: " + isEven(num) );
+
+num = 2;
+console.log("The number " + num + " is even? Answer: " + isEven(num) );
